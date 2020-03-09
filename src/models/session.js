5f3fcc944644223d4a0c39ff2b64173c3b8b6e90
Eugenio Hidalgo Hernández <eugenio.hidalgo@unit4.com>
2020-03-09 11:09:43 +0100

    Added calculated virtual fields on sessions

--- src/models/session.js
+++ src/models/session.js
@@ -8,13 +8,30 @@ var moongoose = require ('mongoose'),
         sessionDate: Date,
         sessionTime: Number,
         sessionDistance: Number,
-        sport: { type: Schema.Types.ObjectId, ref: 'Sports' },
+        sport: { type : Schema.Types.ObjectId, ref: 'Sports' },
         sportName: String,        
-        spot: { type: Schema.Types.ObjectId, ref: 'Spots' },
+        spot: { type : Schema.Types.ObjectId, ref: 'Spots' },
         spotName: String,
+        race: Boolean,
+        indoor: Boolean,
+        value: { type : Number, min: 1, max: 10 },
+        effort: { type : Number, min: 1, max: 10 },
+        maxSpeed: Number,
+        maxPower: Number,
+        medPower: Number,
         active : { type : Boolean, default : true },
         created : { type : Date, default : Date.now },
         updated : { type : Date, default : Date.now }
     });
+
+    SesionSchema.virtual('medSpeed').get(function(){
+        return this.sessionDistance / (this.sessionTime / 3600);
+    });
+
+    SesionSchema.methods.toJSON = function () {
+        var obj = this.toObject();
+        obj.medSpeed = this.medSpeed;
+        return obj;
+     };
 
 module.exports = moongoose.model ('Sessions', SesionSchema);
